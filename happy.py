c4bee89f7997e75ca97aba0e9062f094c3946349
2AMgo <phucthinh.sun@gmail.com>
2022-02-01 09:39:23 +0700

    happy new year

--- happy.py
+++ happy.py
@@ -1,2 +1,1 @@
-print("chuc moi nguoi mot nam moi suc khoe, binh an, hanh phuc")
-print("va nhung dieu tot dep nhat! chuc mung nam moi!")
+print("chuc moi nguoi suc khoe, binh an , hanh phuc trong nam 2022")
